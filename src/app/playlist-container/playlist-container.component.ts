3c55a0f1abe77b48db23c0e0e891161d0984645a
islam ben abba <ei_ben_abba@esi.dz>
2020-11-16 09:15:32 +0100

    changes

--- src/app/playlist-container/playlist-container.component.ts
+++ src/app/playlist-container/playlist-container.component.ts
@@ -14,7 +14,6 @@ export class PlaylistContainerComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, public router: Router) {
   }
-
   ngOnInit(): void {
     this.playlistId$ = this.route.paramMap.pipe(map((params: ParamMap) => parseInt(params.get('id') as string)));
   }
